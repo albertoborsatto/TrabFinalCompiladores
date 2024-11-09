eb39566b0513cfa1b9f6f95c6f22d5be2bc792b7
alberto <albertoborsatto@hotmail.com>
2024-11-09 16:29:19 -0300

    alooo?

--- e3/valor_lexico.h
+++ e3/valor_lexico.h
@@ -1,5 +1,6 @@
 #ifndef _LEX_VALUE_H_
 #define _LEX_VALUE_H_
+#include "parser.tab.h"
 
 typedef enum lex_type {
     LITERAL,
@@ -12,5 +13,7 @@ typedef struct lex_value
     int type;
     char *value;
 } valor_lexico;
+
+valor_lexico create_lex_value(int line_number, int type, char *text);
 
 #endif
