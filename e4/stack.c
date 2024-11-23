825efe18e96b2312aef0b119eb724bd290bb92df
alberto <albertoborsatto@hotmail.com>
2024-11-23 17:39:27 -0300

    this is lepositivity <3

---
+++ e4/stack.c
@@ -0,0 +1,52 @@
+#include "stack.h"
+
+void init_table_stack(table_stack *stack) {
+    stack->tables = malloc(sizeof(symbol_table));
+
+    if (!stack->tables) {
+        perror("Failed to allocate memory for table stack");
+        exit(EXIT_FAILURE);
+    }
+
+     stack->size = 0;
+}
+
+
+void push_table_stack(table_stack *stack, symbol_table *table) {
+    if (stack->size > 0) {
+        stack->tables = realloc(stack->tables, (stack->size + 1) * sizeof(symbol_table));
+    }
+
+    if (!stack->tables) {
+        perror("Failed to reallocate memory for table stack");
+        exit(EXIT_FAILURE);
+    }
+
+    stack->tables[stack->size] = *table;
+    stack->size++;
+}
+
+symbol_table pop_table_stack(table_stack *stack) {
+    if (stack->size == 0) {
+        return;
+    }
+
+    symbol_table popped_table = stack->tables[stack->size - 1];
+    stack->size--;
+
+    stack->tables = realloc(stack->tables, (stack->size) * sizeof(symbol_table));
+
+    if (!stack->tables && stack->size > 0) {
+        perror("Failed to reallocate memory for table stack after pop");
+        exit(EXIT_FAILURE);
+    }
+
+    return popped_table;
+}
+
+void free_table_stack(table_stack *stack) {
+    free(stack->tables);
+    stack->tables = NULL;
+}
+
+
---
+++ e4/stack.h
@@ -0,0 +1,17 @@
+#ifndef _STACK_H_
+#define _STACK_H_
+
+#include <stdlib.h>
+#include "table.h"
+
+typedef struct table_stack {
+    symbol_table *tables; // Array dinâmico de tabelas de símbolos
+    size_t size;          // Número atual de tabelas na pilha
+} table_stack;
+
+void init_table_stack(table_stack *stack);
+void push_table_stack(table_stack *stack, symbol_table *table);
+symbol_table *pop_table_stack(table_stack *stack);
+void free_table_stack(table_stack *stack);
+
+#endif
